cc5592b70ce6e26c6e60e3030fdde4fa3d53c9cd
Alicia Petgrave <alicia.petgrave@gmail.com>
2020-01-31 11:25:59 -0500

    setting up imports and state

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard"
+import Axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [chars, setChars] = useState([])
 
   useEffect(() => {
+    Axios.get("https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      console.log(response.data.results)
+      setChars(response.data.results)
+    })
+    .catch(error =>{
+      console.log(error)
+    })
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
